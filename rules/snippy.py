97696d03162606554de79f49c64e69541040a893
Pierre-Lemee <pier.lemee@gmail.com>
2024-01-09 10:53:37 +0100

    Add snakemake pipeline

---
+++ rules/snippy.py
@@ -0,0 +1,28 @@
+rule snippy:
+    input:
+        clean_R1 = f"{PROJECTNAME}/{{sample}}/fastp/{{sample}}_clean_R1.fastq.gz",
+        clean_R2 = f"{PROJECTNAME}/{{sample}}/fastp/{{sample}}_clean_R2.fastq.gz",
+        genome_ref = config['path_genome_ref']
+    output:
+        variant_tab = f"{PROJECTNAME}/{{sample}}/snippy/{{sample}}_snps.tab"
+    params:
+        outdir = f"{PROJECTNAME}/{{sample}}/snippy/",
+        prefix = f"{{sample}}_snps"
+    log:
+        f"{PROJECTNAME}/logs/{{sample}}/{{sample}}_snippy.log"
+    conda:
+        "../envs/snippy.yaml"
+    threads:
+        config['snippy']['threads']
+    shell:
+        """
+        snippy --cpus {threads} \
+        --force \
+        --outdir {params.outdir} \
+        --prefix {params.prefix} \
+        --ref {input.genome_ref} \
+        --R1 {input.clean_R1} \
+        --R2 {input.clean_R2} \
+        2>{log}
+        """
+    
---
+++ variant_calling.snakefile
@@ -0,0 +1,23 @@
+### Import configfiles
+configfile: "config/config_variant_calling.json" #Config file of the project with all the samples
+configfile: "config/config_tools.json" #Config file of tools options and thresholds.
+
+include: "scripts/functions_variant_calling.py"
+PROJECTNAME = config["project_name"]
+
+### Create folders
+mkdirectory(PROJECTNAME, config['sample'])
+
+### Wildcards
+SAMPLES = get_list_sample(config['sample'])
+
+### Import rules
+include: "rules/fastp_variant.py"
+include: "rules/snippy.py"
+
+list_files = get_all_output(config['sample'])
+
+rule all:
+    input:
+        expand(f"{PROJECTNAME}/{{sample}}/fastp/{{sample}}_fastp.{{extension}}", sample=SAMPLES, extension=["json", "html"]),
+        list_files
